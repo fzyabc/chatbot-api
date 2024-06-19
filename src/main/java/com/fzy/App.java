e6cbdf8a9d3a3dcd977527ff013aed822f641301
fzy <2215697149@qq.com>
2024-06-19 22:09:10 +0800

    小冯  init

---
+++ src/main/java/com/fzy/App.java
@@ -0,0 +1,13 @@
+package com.fzy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
